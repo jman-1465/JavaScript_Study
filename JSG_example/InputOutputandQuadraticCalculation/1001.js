2cb0a69b8944986c0df98ad8005d83a269eec6b0
Jeong_ <81066287+jman-1465@users.noreply.github.com>
2023-03-22 23:47:45 +0900

    0322_study

---
+++ JSG_example/InputOutputandQuadraticCalculation/1001.js
@@ -0,0 +1,5 @@
+//0322
+let input = require('fs').readFileSync('example_1001.txt').toString().split('\n');
+const a = parseInt(input[0]); //parseInt는 입력받은 문자열을 숫자값으로 전환
+const b = parseInt(input[1]);
+console.log(a-b);
---
+++ JSG_example/InputOutputandQuadraticCalculation/2557.js
@@ -0,0 +1,3 @@
+//0319
+//let input = require('fs').readFileSync('example.txt').toString().split('\n');
+console.log("Hello World!");
